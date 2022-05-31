b6a4eb48a264d41b270e197f8b212c93d6aeb38a
CalebRR <yohannagraciano@gmail.com>
2022-05-31 15:05:55 -0300

    DOOM

---
+++ app.js
@@ -0,0 +1,14 @@
+const inputColor = document.querySelector("#inputColor")
+const btnVisualizar = document.querySelector("#btnVisualizar")
+const parrafoExa = document.querySelector("#parrafoExa")
+const cardColor = document.querySelector("#cardColor")
+console.log(inputColor.value);
+
+btnVisualizar.addEventListener("click",()=>{
+    console.log("me diste click");
+    console.log(inputColor.value);
+    parrafoExa.textContent = inputColor.value
+    cardColor.style.backgroundColor = inputColor.value
+
+})
+
